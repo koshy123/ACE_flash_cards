04050baac5378e3e1af84c89d34d9f5d364f48ec
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-10-24 14:55:40 -0400

    Working on the Javascript

--- ace_cards.js
+++ ace_cards.js
@@ -1,1 +1,6 @@
 console.log("hi");
+
+
+let submit = document.createElement("");  // This is creating a button node 
+let submitText = document.createTextNode("Memorize")  // This is creating a text but not stored anywhere
+let tom = document.querySelector("#name"); 
