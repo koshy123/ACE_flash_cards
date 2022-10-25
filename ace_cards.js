ca12bb829e9d2aa7f46e886509aa2348f043832d
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-10-25 14:24:28 -0400

    some work

--- ace_cards.js
+++ ace_cards.js
@@ -6,3 +6,25 @@ let submitText = document.createTextNode("Memorize")  // This is creating a text
 let tom = document.querySelector(".name"); 
 
 submit.addEventListener('click', enterAnswer)
+
+
+
+function newCard(event) {
+    event.preventDefault()
+    tom.innerHTML = " ";
+    fetch('', { })
+    .then(res => { return res })
+    .then(res => { 
+        let card = res.results; 
+        for(let i = 0; i < newCard.length; i++){
+           const card = document.createElement("div");
+           tom.appendChild(newCard)
+           newArea.innerText =  card[i].name + ", "
+           
+           //Need to fix up this line so that the results in the console will desiplay on page
+          // let tom = document.querySelector("#results").createTextNode(res.forms[0]["name"])
+        }       //Only able to get one name to appear on screen right now. Need to get it to run though all parts
+    })
+    .catch(err => { console.log("something went wrong...", err) })    
+}
+
